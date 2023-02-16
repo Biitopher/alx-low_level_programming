4acac2d4c00fbb5da5710abb7604f99e55680905
Biitopher <biitopher@gmail.com>
2023-02-16 04:42:36 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,3 @@
 #include <stdio.h>
-puts("\"Programming is like building a multilingual puzzle\n"); return 0;
+/**
+* int main - {puts("\"Programming is like building a multilingual puzzle\n"); return (0);}
