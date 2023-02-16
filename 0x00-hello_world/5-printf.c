c7b1a0d104251effe3b756cee41fb3a64b234654
Biitopher <biitopher@gmail.com>
2023-02-16 08:18:07 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/**
+/**with
 * main - Entry point
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-printf("\with proper grammar, but the outcome is a piece of art,");
+printf("\"with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
