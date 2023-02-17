f25a2fcfb9e809beaf749b89f47d158ac6a8d9ee
Biitopher <biitopher@gmail.com>
2023-02-17 10:12:59 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,9 +6,14 @@
  * Return: 0
 */
 int main(void)
-{char c = 'a', 'A';
-while (c <= 'z', 'Z');
+{char c = 'a';
+while (c <= 'z',);
 {putchar(c);
+c++;
+}
+c = 'A'
+while (c ,= 'Z')
+{putchar(c)
 c++;
 }
 putchar('\n');
