92c8464e5e138fca96568ee8e4b4b77fb496757d
Biitopher <biitopher@gmail.com>
2023-02-17 07:37:50 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,8 +12,8 @@ n = rand() - RAND_MAX / 2;
 if (n > 0)
 printf("Last digit of %d is 8 and is greater than 5\n", n);
 else if (n == 0)
-printf("%d is zero\n", n);
+printf("Last digit of %d is 0 and is 0\n", n);
 else
-printf("%d is negative\n", n);
+printf("Last digit of %d is -8 and is less than 6 and not 0\n", n);
 return (0);
 }
