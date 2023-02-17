51935a08a8d19676b5393cd94f5f9d5be91f797d
Biitopher <biitopher@gmail.com>
2023-02-17 10:01:03 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Get a random it with S
+ * Return: 0
+*/
+int main(void)
+{char c = 'A';
+while (c <= 'Z')
+{putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
