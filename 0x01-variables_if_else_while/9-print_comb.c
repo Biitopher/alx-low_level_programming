61f1a3c0b6e1856a96d4b0abc613cb24123b8940
Biitopher <biitopher@gmail.com>
2023-02-17 17:04:25 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,11 +6,12 @@
 int main(void)
 {int a;
 for (a = 0; a < 10; a++)
-{putchar(a + '0');
+{
+putchar(a + '0');
 if (a < 9)
 {
-putchar('.');
-putchar('.');
+putchar(',');
+putchar(' ');
 }
 }
 putchar('\n');
