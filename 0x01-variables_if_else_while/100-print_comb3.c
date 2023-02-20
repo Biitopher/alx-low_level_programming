dd4c687a3db255e33493a4cfac41c84496ff0a95
Biitopher <biitopher@gmail.com>
2023-02-20 10:46:37 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  * Return: 0 (success)
  */
 int main(void)
-{int a;
-for (a = 0; a < 100; a++)
+{int a, c;
+for (a = 0; a < 9; a++)
 {
-putchar(a + '0');
-if (a < 99)
+for (c =a+1; c <= '9'; c++)
 {
+if (c != a)
+{
+putchar(a);
+putchar(c);
+if (a == '8' && c == '9')
+continue;
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
