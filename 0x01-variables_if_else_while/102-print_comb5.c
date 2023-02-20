3a9f46be1f4e9f7d9ba714ce8628ca5c8123e622
Biitopher <biitopher@gmail.com>
2023-02-20 11:21:21 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{int a, c;
+for (a = '0'; a < '9'; a++)
+{
+for (c = a + 1; c <= '9'; c++)
+{
+if (c != a)
+{
+putchar(a);
+putchar(c);
+if (a == '8' && c == '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
