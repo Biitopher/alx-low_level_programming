0ff7dc918ca22dd3eafe158735e1441fdbcbe521
Biitopher <biitopher@gmail.com>
2023-02-20 11:31:33 -0800

    vim 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {int a, c;
-for (a = '0'; a < '9'; a++)
+for (a = '0'; a < '98'; a++)
 {
-for (c = a + 1; c <= '9'; c++)
+for (c = a + 1; c <= '99'; c++)
 {
-if (c != a)
-{
-putchar(a);
-putchar(c);
-if (a == '8' && c == '9')
+putchar((a / 10) + '0')
+putchar((a % 10) + '0')
+putchar(' ')
+putchar((c / 10) + '0')
+putchar((q % 10) + '0')
+if (a == '98' && c == '99')
 continue;
 putchar(',');
 putchar(' ');
-}
 }
 }
 putchar('\n');
