ebf8b37d4c1527c67ea977e76c650470a8726242
Biitopher <biitopher@gmail.com>
2023-02-21 04:43:07 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,16 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * main - check the code
  * Return: Always 0.
 */
-int main(void)
+void print_alphabet(void);
 {
-print_alphabet();
+char (c = 'a')
+while (c <= 'z')
+{_putchar(c);
+c++;
+}
+putchar('\n');
 return (0);
 }
