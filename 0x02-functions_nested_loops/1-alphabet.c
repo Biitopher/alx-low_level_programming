c319b678f129d75e4b294381b19fd9c0f60a5366
Biitopher <biitopher@gmail.com>
2023-02-21 04:56:35 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,9 @@
  * Return: Always 0.
 */
 void print_alphabet(void)
-{
-char (c = 'a')
-while (c <= 'z')
+{int c;
+for (c = 'a'; c <= 'z'; c++)
 {_putchar(c);
-c++;
 }
 putchar('\n');
-return (0);
 }
