cf48dfc48491994a67f7e25aab189ec8426ccf60
Biitopher <biitopher@gmail.com>
2023-02-21 13:36:13 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,7 +6,7 @@
 void times_table(void)
 {
 int x, y, z;
-for ( x = 0; x <= 9; x++)
+for (x = 0; x <= 9; x++)
 {
 _putchar('0');
 for (y = 1; y <= 9; y++)
@@ -14,7 +14,7 @@ for (y = 1; y <= 9; y++)
 _putchar(',');
 _putchar(' ');
 z = x + y;
-if (z <= 9)
+if (z < 9)
 _putchar(' ');
 else
 _putchar((z / 10) + '0');
