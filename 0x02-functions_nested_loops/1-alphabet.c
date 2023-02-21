d1158fc0a55d30af5e99c2ff3e132a884bb0f920
Biitopher <biitopher@gmail.com>
2023-02-21 03:50:57 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+*/
+int main(void)
+{
+print_alphabet('a', 'z');
+{
+putchar(',');
+putchar(' ');
+}
+putchar('\n')
+return (0);
+}
