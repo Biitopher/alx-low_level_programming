2a82b22b8a7b88cb01a04f0bc91adb0ad75ea492
Biitopher <biitopher@gmail.com>
2023-02-21 05:18:21 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/*
+ * print_alphabet_x10 -> print the  lowercase alphabets
+ */
+void print_alphabet_x10(void);
+{
+int c;
+int e;
+for (e = '0'; e <= '10'; e++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{_putchar(c);
+}
+putchar('\n')
+}
