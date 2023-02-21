d2322736e60c93096ac68edd211351a340d4b535
Biitopher <biitopher@gmail.com>
2023-02-21 07:31:22 -0800

    int print_sign(int n)

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_sign -> sign of a number
+* @c: a character arguement
+* Return: returns 1 , 0 and -1 depending on condition
+*/
+int print_sign(int n)
+{
+return ((n > 0) || (n = 0) || (n < 0));
+}
