d5512e3fa3c155dd6f7d054aff2e923434246f40
Biitopher <biitopher@gmail.com>
2023-02-22 07:49:13 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,1 +1,21 @@
-
+#incude <stdio.h>
+#include <main.h>
+/**
+ *print_times_table -  prints the n times table, starting with 0
+*/
+void print_times_table(int n)
+{
+	int n;
+	if (n > 15)
+	{printf("%d", n);
+	printf("\n");
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	if (n < 0)
+		printf("%d", n);
+	printf("\n");
+}
+return (0);
+}
