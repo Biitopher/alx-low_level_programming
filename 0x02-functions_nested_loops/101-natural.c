f268ce68a90cdae3a74af07101b81da4725b48d3
Biitopher <biitopher@gmail.com>
2023-02-22 15:47:03 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include  "main.h"
+/**
+ * Nature made the natural numbers
+ * Return: 0 (success)
+ */
+main int(void)
+{
+int i ; j = 0;
+while (i = 1024)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+{
+j += i;
+}
+i++;
+}
+printf("%d\n", j);
+return (0);
+}
