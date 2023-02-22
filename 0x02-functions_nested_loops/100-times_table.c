effb5f8b8391694597a0e845a19dbbd2024c260d
Biitopher <biitopher@gmail.com>
2023-02-22 11:46:58 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,19 +6,17 @@
 */
 void print_times_table(int n)
 {
-if
-(n > 15)
-for (n = 0; n > 15; n++)
-{
-printf("%d", n);
-printf("\n");
-{
-putchar(',');
-putchar(' ');
+ if (n >= 98)
+        {
+                while (n > 15)
+                        printf("%d, ", n--);
+                printf("%d\n", n);
+        }
+        else
+        {
+                while (n < 0)
+                        printf("%d, ", n++);
+                printf("%d\n", n);
+        }
 }
-if (n < 0)
-for (n = 0; n < 0; n--)
-printf("%d", n);
-printf("\n");
-}
-}
+
