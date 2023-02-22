48d6166d30a0b515a2dbb7ac4d125cb3a0b6ba6c
Biitopher <biitopher@gmail.com>
2023-02-21 16:06:40 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@ void print_to_98(int n)
 {
 if (n <= 98)
 {
-for (; n <=98; n++)
+for (; n <= 98; n++)
 {
 if (n == 98)
 {
@@ -17,8 +17,10 @@ printf("%d", n);
 printf("\n");
 break;
 }
-else 
+else
 {
+printf("%d, "' n);
+}
 for (; n >= 98; n--)
 {
 if (n == 98)
