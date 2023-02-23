385eccd16be5895528598c5b428d82ad748e72e1
Biitopher <biitopher@gmail.com>
2023-02-23 08:47:59 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <studio.h"
+
+/** -isdigi - checks for a digit
+ * @c: checks if it is a digit 
+ * Return: return 1 if character is a digit and 0 if else
+*/
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <=57)
+{
+return (1);
+}
+return (0);
+}
