10d76a422a2adff493d91bcde1cd6a62e58ac95d
Biitopher <biitopher@gmail.com>
2023-02-27 12:57:46 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,9 @@
 void print_rev(char *s)
 {
 int c = 0;
-while
-(s[c] != '\0')
-{
+while (s[c] != '\0')
 c++;
-}
-for
-(c = c - 1; c >= 0; c--)
-{
+for (c = c - 1; c >= 0; c--)
 putchar(s[c]);
-}
 putchar('\n');
 }
