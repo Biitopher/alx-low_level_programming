ceb3fe8f73f446d18a7b177e7bade86e08caba01
Biitopher <biitopher@gmail.com>
2023-02-27 09:08:27 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+/**
+ * rev_string - function that reverses a string
+ * @s: reverses string
+ * Return: Reverse.
+ */
+
+void rev_string(char *s)
+{
+char rv = s[0];
+int a = 0;
+int b;
+while (s[a] != '\0')
+c++;
+for
+(b = 0; b < a; b++)
+{
+a--;
+rv = s[b];
+s[b] = s[a];
+s[a] = rv;
+}
+}
