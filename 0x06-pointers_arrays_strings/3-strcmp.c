ad38a4b47543f3b8a20f0ca0db204645779ef3bb
Biitopher <biitopher@gmail.com>
2023-03-01 12:51:55 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int s1, s2;
-s1 = 0;
-while (s1 < s2 != '\0')
+int c;
+c = 0;
+while (s1[c] != '\0' && s2[c] != '\0')
 {
-s1++;
+if (s1[c] != s2[c])
+{
+return (s1[c] = s2[c]);
 }
-s2 = 0;
-while (s2 < s1 != '\0')
-{
-s2++;
+c++;
 }
 return 0;
 }
