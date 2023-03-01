e075a1f7b88e19292c90076e48cdb5809e9cc59e
Biitopher <biitopher@gmail.com>
2023-03-01 14:02:20 -0800

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,14 +12,11 @@ void reverse_array(int *a, int n)
 {
 int i;
 int temp;
-for (i = 0, i < n--;)
-{
-i++;
-}
+for (i = 0; i < n--; i++)
 {
 temp = a[i];
 a[i] = a[n];
 a[n] = temp;
 }
-return 0;
+return (0);
 }
