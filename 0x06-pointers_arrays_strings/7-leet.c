62856fe3ec43dd3fc1b0001530905f9652d2691c
Biitopher <biitopher@gmail.com>
2023-03-02 07:56:46 -0800

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *letter - function that encodes a string into1337
+ *@n: parameter
+ *Return: string
+ */
+
+char *letter(char *n)
+{
+int a = 0, b, l = 5;
+char tr[5] = {'A', 'E', 'O', 'T', 'L'};
+char trw[5] = {'4', '3', '0', '7', '1'};
+while (n[a])
+{
+b = 0;
+while (b < l)
+{
+if (n[a] == tr[b] || n[a] - 32 == tr[b])
+n[a] = trw[b];
+b++;
+}
+a++;
+}
+return (n);
+}
