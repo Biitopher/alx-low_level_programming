9e1d196291ac0db286097da6ec0663bf480273bf
Biitopher <biitopher@gmail.com>
2023-03-02 17:43:30 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,17 +4,19 @@
 /**
  *print_number - function that prints an integer
  *@n: integer parameter
-  */
+ *Return: 0
+ */
 
 void print_number(int n)
 {
-unsigned int n1 = n;
+unsigned int n1
+n1 = n;
 if (n < 0)
 {
 putchar('-');
-n1 = -n1;
+n1 = -n;
 }
-if ((n1 / 10) > 0)
+if (n1 / 10 !=  0)
 {
 print_number(n1 / 10);
 }
