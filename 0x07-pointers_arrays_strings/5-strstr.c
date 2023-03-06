3a7d43b7a62c34bc5d22cd5e3498c6242f33243b
Biitopher <biitopher@gmail.com>
2023-03-06 10:18:49 -0800

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - function that locates a substring
+ * @haystack: first occurence in string
+ * @needle: first occurence in substring
+ * Return: o success or null if not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+for (; *haystack != '\0'; haystack++)
+{
+char i = *haystack;
+char k = *needle;
+while (i == k && k != '\0')
+{
+i++;
+k++;
+}
+if (k == '\0')
+return (haystack);
+}
+return (0);
+}
