c68e39493d1d9689ea7e04d5884f6c5d5373e57a
Biitopher <biitopher@gmail.com>
2023-03-06 08:08:08 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - writes character c to stdout
+ * @c: character to print
+ * Return 1 success
+ */
+
+int _putchar(char c)
+{
+return (write(1, &c, 1))
+}
