34f1d73e3a82a71be67be7ce775ea45bfcc722de
Biitopher <biitopher@gmail.com>
2023-03-06 12:46:04 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,14 +12,14 @@ char *_strstr(char *haystack, char *needle)
 {
 for (; *haystack != '\0'; haystack++)
 {
-char i = *haystack;
-char k = *needle;
-while (i == k && k != '\0')
+char *i = haystack;
+char *k = needle;
+while (*i == *k && *k != '\0')
 {
 i++;
 k++;
 }
-if (k == '\0')
+if (*k == '\0')
 return (haystack);
 }
 return (0);
