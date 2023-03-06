f30182176e9eed0f4240d7383817abbd844adfc5
Biitopher <biitopher@gmail.com>
2023-03-06 12:01:04 -0800

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,7 +13,7 @@ void print_diagsums(int *a, int size)
 int c, n;
 int i = 0, j = 0;
 for (c = 0; c <= (size * size); c = c + size + 1)
-i = i + a[i];
+i = i + a[c];
 for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
 j = j + a[n];
 printf("%d, %d\n", i, j);
