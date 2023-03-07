3b437ece132f60fa4fe257dcb138639d823b213e
Biitopher <biitopher@gmail.com>
2023-03-07 11:44:44 -0800

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -3,12 +3,11 @@
 
 /**
  * set_string - function that sets the value of a pointer to a char
- * @s: string
+ * @s: pointer to pointer
  * @to: input
- * Return: 0 success
  */
 
 void set_string(char **s, char *to)
 {
-**s = *to;
+*s = to;
 }
