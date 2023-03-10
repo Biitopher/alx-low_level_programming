392ab8304aa59e26f3d373bbf6f7f60910b836aa
Biitopher <biitopher@gmail.com>
2023-03-10 11:06:06 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints its name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 success
+ */
+
+int main(char *argv, int argc)
+{
+printf("%s\n", *argv);
+return 0;
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int main(char *argv, int argc);
+#endif /* MAIN_H */
