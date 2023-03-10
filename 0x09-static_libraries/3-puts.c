15e9b908c2738597cb0ab18a77d5d4c3112e6e3c
Biitopher <biitopher@gmail.com>
2023-03-10 08:45:14 -0800

    libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * @*str: string
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+{
+for
+(; *str != '\0'; str++)
+while (*str != '\0')
+{
+putchar(*str);
+putchar(*str++);
+}
+putchar('\n');
+}
