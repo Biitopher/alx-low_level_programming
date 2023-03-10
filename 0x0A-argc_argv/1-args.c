4e697bc7f8c63c3e3db019be758012f982608ff3
Biitopher <biitopher@gmail.com>
2023-03-10 12:11:55 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,14 +3,14 @@
 
 /**
  * main - prints the number of arguments passed into it
- *@argc: number of argument
- *@argv: array
+ * @argc: number of argument
+ * @argv: array
  * Return: 0 success
  */
 
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
 (void)(argv);
-printf("d\n", argc - 1);
+printf("%d\n", argc - 1);
 return (0);
 }
