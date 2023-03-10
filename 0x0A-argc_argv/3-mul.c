d8d5bf4001a16fa6b9fc713676d913f4ba8b921b
Biitopher <biitopher@gmail.com>
2023-03-10 14:25:44 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,7 +5,7 @@
  * main - program that multiplies two numbers
  * @argc: number of arguments
  * @argv: array argument
- * Return: 0 success, 1 error
+ * Return: 0 success, 1 Error if not
  *
  */
 
@@ -17,9 +17,9 @@ if (argc < 3 || argc > 3)
 printf("Error\n");
 return (1);
 }
-i =  _atoi(argv[1]);
-j =  _atoi(argv[2]);
-len = i *j;
+i =  atoi(argv[1]);
+j =  atoi(argv[2]);
+len = i * j;
 printf("%d\n", len);
 return (0);
 }
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that adds positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 successs, 1 Error
+ */
+
+
