4bf83790faa7e8a9a07d586632ca3ab6e17261a0
Biitopher <biitopher@gmail.com>
2023-03-13 12:38:14 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars, and initializes it
+ * @size: size of the memory to print
+ * @c: array of chars
+ * Return: 0 if success or else null if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+char *str;
+unsigned int i;
+str = malloc(sizeof(char) * size);
+if (size == 0 || str == NULL)
+return (NULL);
+if (i = 0; i < size; i++)
+str[i] = c;
+return (str);
+}
