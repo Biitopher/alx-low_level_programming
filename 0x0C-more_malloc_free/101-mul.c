d502a05c2d630cf2da18c13406592b2a75ee39ef
Biitopher <biitopher@gmail.com>
2023-03-16 16:01:20 -0700

    header

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * main - multiplies two positive numbers
+ * @num1: first integer
+ * @num2: second integer
+ * Return: 0 success
+ */
+
+int main(int num1, int num2)
+{
+
+}
