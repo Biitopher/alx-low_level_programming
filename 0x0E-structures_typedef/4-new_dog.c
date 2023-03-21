dd273649ae14fd189c300605814ae780b0d744b9
Biitopher <biitopher@gmail.com>
2023-03-20 19:55:19 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,3 +18,60 @@ a++;
 return (a);
 }
 
+ /**
+ * *_strcpy - copies the string
+ * @dest: destination of string
+ * @src: source of string
+ * Return: to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int len, a;
+len = 0;
+while (src[len] != '\0')
+{
+len++;
+}
+for (a = 0; a < len; a++)
+{
+dest[a] = src[a];
+}
+dest[a] = '\0';
+return (dest);
+}
+
+/**
+* new_dog - function that creates a new dog
+* @name: first element
+* @age: second element
+* @owner: third element
+*
+* Return: new dog success, null if not
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *dog;
+int len1, len2;
+len1 = _strlen(name);
+len2 = _strlen(owner);
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
+dog->name = malloc(sizeof(char) * (len1 + 1));
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
+}
+dog->owner = malloc(sizeof(char) * (len2 + 1));
+if (dog->owner == NULL)
+{
+free(dog);
+free(dog->name);
+return (NULL);
+}
+_strcpy(dog->name, name);
+_strcpy(dog->owner, owner);
+dog->age = age;
+return (dog);
+}
