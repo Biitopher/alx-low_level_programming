2a49806823b952813f8162499655d52ebf0b1268
Biitopher <biitopher@gmail.com>
2023-03-23 05:29:03 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,28 +11,18 @@
 
 int main(int argc, char *argv[])
 {
-int bytes, i;
-char *a;
+int i;
 if (argc != 2)
 {
 printf("Error\n");
 exit(1);
 }
-bytes = atoi(argv[1]);
-if (bytes < 0)
+i = atoi(argv[1]);
+if (i < 0)
 {
 printf("Error\n");
 exit(2);
 }
-a = (char *)main;
-for (i = 0; i < bytes; i++)
-{
-if (i == bytes - 1)
-{
-printf("%02hhx\n", a[i]);
-break;
-}
-printf("%02hhx", a[i]);
-}
+print_opcodes((char *)&main, i);
 return (0);
 }
