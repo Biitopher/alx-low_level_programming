48b80bbc25a3d68b6e29c082f7ca8dc9246c21f2
Biitopher <biitopher@gmail.com>
2023-04-14 12:13:38 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -51,7 +51,7 @@ int a, b, from, to;
 char *buff;
 if (argc != 3)
 {
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n", argv[0]);
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 buff = create_buffer(argv[2]);
