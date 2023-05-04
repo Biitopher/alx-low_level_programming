5c3c2a72e38e27b83832a8d88b4ac51211f0e542
root <biitopher@gmail.com>
2023-05-04 06:00:57 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,10 +16,11 @@ unsigned int binary_to_uint(const char *b)
 	{
 	return (0);
 	}
-	for (x = 0; b[x] != '\0';)
+	for (x = 0; b[x];)
 	x++;
-	if (b[x] == '0' || b[x] == '1')
 	{
+	if (b[x] < '0' || b[x] > '1')
+		return (0);
 		uint <<= 1;
 		uint += b[x] - '0';
 	}
