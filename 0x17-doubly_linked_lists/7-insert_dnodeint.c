1efe8eccef5401f52f70acf1faf4d768a2c647ec
Biitopher <biitopher@gmail.com>
2023-06-15 15:59:31 -0700

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,9 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new_node->prev = NULL;
 		new_node->next = *h;
 		if (*h != NULL)
-		{
 			(*h)->prev = new_node;
-		}
 		*h = new_node;
 	}
 	else
@@ -42,16 +40,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			free(new_node);
 			return (NULL);
-
+		}
 			new_node->prev = current;
 			new_node->next = current->next;
 
 			if (current->next != NULL)
-			{
 				current->next->prev = new_node;
-			}
 			current->next = new_node;
-		}
 	}
 		return (new_node);
 }
