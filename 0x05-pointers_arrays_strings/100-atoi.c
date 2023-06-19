208e164ded0843db7eea84ebe0a9fa1f5d5ac463
Biitopher <biitopher@gmail.com>
2023-06-19 03:55:08 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,31 +9,33 @@
 
 int _atoi(char *s)
 {
-int sign = 1;
 int result = 0;
-int i = 0;
+int sign = 1;
+int digit;
 
-while (s[i] == ' ')
-i++;
-if (s[i] == '-')
+while (*s == ' ')
+s++;
+if (*s == '-')
 {
 sign = -1;
-i++;
+s++;
 }
-else if (s[i] == '+')
+else if (*s == '+')
 {
-i++;
+s++;
 }
-while (s[i] >= '0' && s[i] <= '9')
+while (*s >= '0' && *s <= '9')
 {
-if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
+digit = *s - '0';
+if (result > (INT_MAX - digit) / 10)
 {
-if (sign == -1)
-return (INT_MIN);
-else
+if (sign == 1)
 return (INT_MAX);
+else
+return (INT_MIN);
 }
-result = result * 10 + (s[i] - '0');
-i++;
+result = result * 10 + digit;
+s++;
+}
 return (sign *result);
 }
