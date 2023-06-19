0d5fb70e9f9b835bd4318fa821ac50e276fc9702
Biitopher <biitopher@gmail.com>
2023-06-19 12:24:28 -0700

    libdynamic.so

--- 0x18-dynamic_libraries/0-putchar.c
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -5,15 +5,6 @@
  */
 int main(void)
 {
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('\n');
 return (0);
 }
 
--- 0x18-dynamic_libraries/100-atoi.c
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -1,4 +1,4 @@
-#include <limits.h>
+#include "main.h"
 
 /**
  * _atoi - a function that convert a string to an integer
@@ -9,33 +9,5 @@
 
 int _atoi(char *s)
 {
-int result = 0;
-int sign = 1;
-int digit;
-
-while (*s == ' ')
-s++;
-if (*s == '-')
-{
-sign = -1;
-s++;
-}
-else if (*s == '+')
-{
-s++;
-}
-while (*s >= '0' && *s <= '9')
-{
-digit = *s - '0';
-if (result > (INT_MAX - digit) / 10)
-{
-if (sign == 1)
-return (INT_MAX);
-else
-return (INT_MIN);
-}
-result = result * 10 + digit;
-s++;
-}
-return (sign *result);
+return (0);
 }
