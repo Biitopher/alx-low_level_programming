40dbde15ea2080678893b3e79115ac1a76c5a237
Biitopher <biitopher@gmail.com>
2023-06-20 11:27:48 -0700

    100-operations.so

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int add_numbers(int a, int b)
+{
+    return (a + b);
+}
+
+int sub_numbers(int a, int b)
+{
+    return (a - b);
+}
+
+int mul_numbers(int a, int b)
+{
+    return (a * b);
+}
+
+int div_numbers(int a, int b)
+{
+    return (a / b);
+}
+
+int mol_numbers(int a, int b)
+{
+    return (a % b);
+}
