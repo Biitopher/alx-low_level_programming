8c99fdb0c1f623d4d0ea11e01ee9c71768fb2a4b
Biitopher <biitopher@gmail.com>
2023-07-06 04:49:48 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ typedef struct
 	hash_table_t;
 }
 
-hash_table_t *hash_table_create(unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size)
 {
 	 hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	 if (table == NULL)
