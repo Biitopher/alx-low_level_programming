15f48478435a8e7ed995839b3a677766302c9483
Biitopher <biitopher@gmail.com>
2023-07-06 06:16:14 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,31 +7,25 @@
  * Return: pointer to newly created hash table, else NULL
  */
 
-typedef struct
-{
-	hash_table_t;
-}
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	 hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
-	 if (table == NULL)
+	unsigned long int x;
+	hash_table_t *table;
+	table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
 	 {
-		 return NULL;
+		 return (NULL);
 	 }
-	 return table;
-}
 
-int main()
-{
-    unsigned long int size = 100;
-    hash_table_t *table = hash_table_create(size);
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) *size);
 
-    if (table == NULL) {
-        printf("Failed to create hash table\n");
-        return 1;
-    }
-    free(table);
-
-    return 0;
+	if (table->array == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; x < size; x++)
+		table->array[x] = NULL;
+	return (table);
 }
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,46 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to the hash table.
+ * @ht: hash table to be added or updated
+ * @value: value associated with the key 
+ *
+ * Return: 1 on success, 0 on fail
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	char *copy_value;
+	unsigned long int idx, a;
+
+	if (key == NULL || ht == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	copy_value = strdup(value);
+	if (copy_value == NULL)
+		return (0);
+	idx = key_index((const unsigned char *)key, ht->size);
+	for (a = idx; ht->array[a]; a++)
+	{
+		if (strcmp(ht->array[a]->key, key) == 0)
+		{
+			free(ht->array[a]->value);
+			ht->array[a]->value = copy_value;
+			return (1);
+		}
+	}
+
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(copy_value);
+		return (0);
+	}
+	new->value = copy_value;
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
+	return (1);
+}
+
+
