b8865f7db68d5b4087960688fdb682a8746691d2
Biitopher <biitopher@gmail.com>
2023-08-23 11:07:58 -0700

    Task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Function that defines island perimeter grid returns perimeter of the island"""
+
+def island_perimeter(grid):
+    """Returns perimeter of the island"""
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
