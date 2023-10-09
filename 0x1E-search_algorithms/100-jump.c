e852bfa08f0823b67dd3286c3db0cf1e35fdfb60
Biitopher <biitopher@gmail.com>
2023-10-09 15:08:41 -0700

    Task 14

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -17,7 +17,7 @@ int jump_search(int *array, size_t size, int value)
 	int step = sqrt(size);
 	int prev = 0;
 	int i;
-	
+
 	if (array == NULL || size == 0)
 		return (-1);
 	printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
@@ -29,12 +29,11 @@ int jump_search(int *array, size_t size, int value)
 		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 	}
 	printf("Value found between indexes [%d] and [%d]\n", prev - step, prev);
-
-    for (i = prev - step; i < (int)size && array[i] <= value; i++)
-    {
-	    printf("Value checked array[%d] = [%d]\n", i, array[i]);
-	    if (array[i] == value)
-		    return (i);
-    }
-    return (-1);
+	for (i = prev - step; i < (int)size && array[i] <= value; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
