f8d4882fbdab0b22b765a54213074d9052b8e5de
Biitopher <biitopher@gmail.com>
2023-10-09 14:55:32 -0700

    Task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,38 +10,25 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i;
-	size_t left = 0;
-	size_t right = size - 1;
-	size_t mid = left + (right - left) / 2;
+	size_t i = 0;
+	int *x = array;
 
-	if (array == NULL || size == 0)
-	{
+	if (!array)
 		return (-1);
-	}
-	while (left <= right)
+	while (size)
 	{
-		printf("Searching in array: ");
-	for (i = left; i <= right; i++)
-		printf("%d", array[i]);
-            if (i < right)
-	    {
-                printf(", ");
+		for (i = 0, printf("Searching in array: "); i < size; i++)
+			printf("%d%s", x[i], i + 1 == size ? "\n" : ", ");
+		i = (size - 1) / 2;
+		if (x[i] == value)
+			return ((x - array) + i);
+		else if (x[i] > value)
+			size = i;
+		else
+		{
+			x += (i + 1);
+			size -= (i + 1);
+		}
 	}
-	printf("\n");
-
-	if (array[mid] == value)
-		printf("Found %d at index: %lu\n", value, mid);
-	return (mid);
-        }
-	if(array[mid] < value)
-	{
-		left = mid + 1;
-	}
-	else
-	{
-		right = mid - 1;
-	}
-	printf("Value %d not found in the array\n", value);
 	return (-1);
 }
